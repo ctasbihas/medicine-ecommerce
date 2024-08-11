56eb9bbb215d980780d31527192bd595eec36071
ctasbihas <ctasbihas@gmail.com>
2024-08-11 09:09:24 +0600

    navbar added

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import Navbar from "@/components/Navbar/Navbar";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "MedZ - Your Trusted Online Medical Store",
+	description:
+		"MedZ is a comprehensive e-commerce platform providing a seamless online shopping experience for medical products, including prescription medications, health supplements, and medical devices. Enjoy a user-friendly interface, secure checkout, and personalized features for a superior shopping experience.",
+	keywords:
+		"medicine, pharmacy, online pharmacy, prescription drugs, health products",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<div className="h-screen bg-gray-600">
+			<h1>Hello World</h1>
+		</div>
+	);
+};
+
+export default Home;
